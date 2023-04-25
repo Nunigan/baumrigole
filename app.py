d3908bb781a3845ce4b4189c352b77c2c612c326
Michael Schmid <michael.schmid2@ost.ch>
2023-04-25 10:48:51 +0200

    added own web to server

--- app.py
+++ app.py
@@ -1,33 +1,64 @@
-import os
-import numpy
-
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import Flask, render_template, Response, request, send_file
+import cv2
+# from imutils.video import VideoStream
+import os 
+from datetime import datetime
+import shutil
+import glob
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    filenames = glob.glob("data/*.csv")
+    for i, files in enumerate(filenames):
+        filenames[i] = files[5:]
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return render_template('index.html',data=filenames)
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+@app.route("/test" , methods=['GET', 'POST'])
+def test():
+    select = request.form.get('file_select')
+    return send_file("../data/{}".format(select), as_attachment=True)
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+@app.route('/show/')
+def show():
+    fileList = glob.glob('static/temp2023*', recursive=True)
+    for filePath in fileList:
+        try:
+            os.remove(filePath)
+        except OSError:
+            print("Error while deleting file")
+    
+    name = 'temp{}.jpg'.format(datetime.today().strftime('%Y_%m_%d_%H:%M:%S'))
+    shutil.copyfile('static/temp.jpg', 'static/'+name)
+    return render_template('show.html',  user_image=name)
 
+# @app.route('/stream/')
+# def stream():
+#     return Response(gather_img(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/plot/')
+def plot():
+    return Response(gather_plot(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# def gather_img():
+#     cap = VideoStream('http://icai:icai@169.254.179.163/mjpg/video.mjpg').start()
+#     while True:
+#         img = cap.read()
+#         _, frame = cv2.imencode('.jpg', img)
+#         yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
+#         time.sleep(0.2)
+
+def gather_plot():
+
+    img = cv2.imread('flask/static/temp.jpg')
+    cv2.imwrite('flask/static/test.jpg', img)
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 100]
+    _, frame = cv2.imencode('.jpg', img, encode_param)
+    yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
 
 if __name__ == '__main__':
-   app.run()
+
+    app.run()
