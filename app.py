9fa2dd613524a6cd9a66edeb15f67a1926320bd4
Michael Schmid <michael.schmid2@ost.ch>
2023-07-13 09:24:12 +0200

    test plotly on webseerver

--- app.py
+++ app.py
@@ -1,115 +1,3 @@
-# from flask import Flask, render_template, Response, request, send_file
-# # import cv2
-# # from imutils.video import VideoStream
-# import glob
-# from azure.identity import DefaultAzureCredential
-# from azure.storage.blob import BlobServiceClient
-# # from plot import plot
-# import time
-
-# # def dowload_cloud():
-# #     t = time.time()
-# #     print('Download from cloud')
-# #     account_url = "https://baumrigole.blob.core.windows.net"
-# #     default_credential = DefaultAzureCredential()
-# #     blob_service_client = BlobServiceClient(account_url, credential=default_credential)
-# #     container_name = 'baumrigoledata'
-# #     container_client = blob_service_client.get_container_client(container=container_name)
-
-# #     filenames_local = sorted(glob.glob("data/*.csv"))
-
-# #     blob_list = container_client.list_blobs()
-# #     for blob in blob_list:
-# #         if 'data' in blob.name:
-# #             if blob.name in filenames_local:
-# #                 if os.stat(blob.name).st_size == blob.size:
-# #                     continue
-# #             name = blob.name
-# #             with open(file=name, mode="wb") as download_file:
-# #                 download_file.write(container_client.download_blob(blob.name).readall())
-# #     print(time.time() - t)
-
-
-
-# app = Flask(__name__)
-# app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     # t = time.time()
-#     # plot()
-#     # print(time.time() - t)
-#     filenames = sorted(glob.glob("/data/data/*.csv"))
-#     filenames = glob.glob("/data/data/*.csv")
-#     for i, files in enumerate(filenames):
-#         filenames[i] = files[11:]
-
-#     return render_template('index.html',data=filenames)
-
-# @app.route("/test" , methods=['GET', 'POST'])
-# def test():
-#     select = request.form.get('file_select')
-#     return send_file("/data/data/{}".format(select), as_attachment=True)
-
-# @app.route('/show/')
-# def show():
-#     # fileList = glob.glob('static/temp2023*', recursive=True)
-#     # for filePath in fileList:
-#     #     try:
-#     #         os.remove(filePath)
-#     #     except OSError:
-#     #         print("Error while deleting file")
-    
-#     # name = 'temp{}.jpg'.format(datetime.today().strftime('%Y_%m_%d_%H:%M:%S'))
-#     shutil.copyfile('/data/data/temp.jpg', 'static/temp.jpg')
-#     shutil.copyfile('/data/data/cam.jpg', 'static/cam.jpg')
-#     shutil.copyfile('/data/data/climavue.jpg', 'static/climavue.jpg')
-#     shutil.copyfile('/data/data/soilvue.jpg', 'static/soilvue.jpg')
-
-#     # shutil.copyfile('../../data/temp.jpg', 'static/temp.jpg')
-#     # shutil.copyfile('../../data/cam.jpg', 'static/cam.jpg')
-#     # shutil.copyfile('../../data/climavue.jpg', 'static/climavue.jpg')
-#     # shutil.copyfile('../../data/soilvue.jpg', 'static/soilvue.jpg')
-
-#     temp = 'temp.jpg'
-#     cam = 'cam.jpg'
-#     soil = 'soilvue.jpg'
-#     clima = 'climavue.jpg'
-
-#     return render_template('show.html', temp=temp, cam=cam, soil=soil, clima=clima)
-#     # return render_template('show.html')
-
-# # @app.route('/stream/')
-# # def stream():
-# #     return Response(gather_img(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-# # @app.route('/plot/')
-# # def plot():
-# #     return Response(gather_plot(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-# # def gather_img():
-# #     cap = VideoStream('http://icai:icai@169.254.179.163/mjpg/video.mjpg').start()
-# #     while True:
-# #         img = cap.read()
-# #         _, frame = cv2.imencode('.jpg', img)
-# #         yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
-# #         time.sleep(0.2)
-
-# # def gather_plot():
-
-# #     img = cv2.imread('flask/static/temp.jpg')
-# #     cv2.imwrite('flask/static/test.jpg', img)
-# #     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 100]
-# #     _, frame = cv2.imencode('.jpg', img, encode_param)
-# #     yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
-
-# if __name__ == '__main__':
-
-#     app.run()
-#     # dowload_cloud()
-
-
-
 from flask import Flask, render_template, request, send_file
 import pandas as pd
 import json
@@ -126,9 +14,6 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # t = time.time()
-    # plot()
-    # print(time.time() - t)
     filenames = sorted(glob.glob("/data/data/*.csv"))
     filenames = glob.glob("/data/data/*.csv")
     for i, files in enumerate(filenames):
@@ -138,30 +23,19 @@ def index():
 
 @app.route('/show/')
 def show():
-    # fileList = glob.glob('static/temp2023*', recursive=True)
-    # for filePath in fileList:
-    #     try:
-    #         os.remove(filePath)
-    #     except OSError:
-    #         print("Error while deleting file")
-    
-    # name = 'temp{}.jpg'.format(datetime.today().strftime('%Y_%m_%d_%H:%M:%S'))
+
     shutil.copyfile('/data/data/temp.jpg', 'static/temp.jpg')
     shutil.copyfile('/data/data/cam.jpg', 'static/cam.jpg')
     shutil.copyfile('/data/data/climavue.jpg', 'static/climavue.jpg')
     shutil.copyfile('/data/data/soilvue.jpg', 'static/soilvue.jpg')
     shutil.copyfile('/data/data/stats.jpg', 'static/stats.jpg')
 
-    # shutil.copyfile('../../data/temp.jpg', 'static/temp.jpg')
-    # shutil.copyfile('../../data/cam.jpg', 'static/cam.jpg')
-    # shutil.copyfile('../../data/climavue.jpg', 'static/climavue.jpg')
-    # shutil.copyfile('../../data/soilvue.jpg', 'static/soilvue.jpg')
-
     temp = 'temp.jpg'
     cam = 'cam.jpg'
     soil = 'soilvue.jpg'
     clima = 'climavue.jpg'
     stats = 'stats.jpg'
+
     return render_template('show.html', temp=temp, cam=cam, soil=soil, clima=clima, stats=stats)
 
 @app.route("/test" , methods=['GET', 'POST'])
@@ -185,9 +59,9 @@ def get_fig():
     df = pd.read_csv('/data/data/all.csv', header=0, parse_dates=[0], index_col=[0])
     rain = df['Rain_mm_Tot']
     strikes = df['Strikes_Tot']
-    df = df.resample('10T').mean().ffill()
-    rain = rain.resample('10T').sum()
-    strikes = strikes.resample('10T').sum()
+    df = df.resample('60T').mean().ffill()
+    rain = rain.resample('60T').sum()
+    strikes = strikes.resample('60T').sum()
 
     fig = make_subplots(rows=5, cols=1, shared_xaxes=True
                         ,specs=[[{"secondary_y": True}], 
@@ -217,7 +91,6 @@ def get_fig():
             go.Scatter(x=df.index, y=df['T_{}_Avg'.format(s)], mode='lines', name='Temp @ {}'.format(s))
             , row=3, col=1)
 
-
     for s in ["5cm","10cm","20cm","30cm","40cm","50cm"]:
         fig.append_trace(
             go.Scatter(x=df.index, y=df["VWC_{}_Avg".format(s)], mode='lines', name='VWC @ {}'.format(s))
@@ -229,9 +102,8 @@ def get_fig():
     x = df_2d.index
 
     fig.append_trace(
-        go.Contour(z=z,x=x,y=y,colorscale='Blues', showscale=False,contours=dict(showlabels = True), name='VWC'),
+        go.Contour(z=z,x=x,y=y,colorscale='Blues', name='VWC',colorbar=dict(len=0.18, y=0.09)),
             row=5, col=1)
-
 
     fig.update_yaxes(title_text=r"VWC (m^3/m^3)", row=4, col=1)
     fig.update_yaxes(title_text=r"Depth (cm)", row=5, col=1)
@@ -243,7 +115,7 @@ def get_fig():
     fig.update_yaxes(autorange="reversed", row=5, col=1)
     fig.update_xaxes(showticklabels=True) 
 
-    fig.update_layout(height=2000, width=1700, title='Weather station and soil water content profile sensor', template='plotly_dark', font=dict(
+    fig.update_layout(height=2000, title='Weather station and soil water content profile sensor', template='plotly_dark', font=dict(
         size=16,
     ))
 
@@ -280,7 +152,7 @@ def get_fig_select():
             fig.add_trace(
                 go.Scatter(
                     x = rain.index,
-                    y = rain,
+                    y = rain*6,
                     name = column
                 )
             )
@@ -362,7 +234,7 @@ def get_fig_select():
                     args = [{'visible': vp},
                             {'title': 'Vapor Pressure (mbar)',
                             'showlegend':True}]),
-            dict(label = 'Strikes',
+                dict(label = 'Strikes',
                     method = 'update',
                     args = [{'visible': strike},
                             {'title': 'Lightning strikes (tot)',
@@ -376,7 +248,7 @@ def get_fig_select():
             )
         ])
     
-    fig.update_layout(template='plotly_dark', font=dict(
+    fig.update_layout(template='plotly_dark', height=1000, font=dict(
         size=16,
     ))
 
